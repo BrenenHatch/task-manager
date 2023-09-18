473ea503803d8e7838839dcc2afe1489d8e9e5f3
Brenen Hatch <brenen.hatch21@gmail.com>
2023-09-18 16:49:27 -0400

    Modified index to use async/await

---
+++ playground/9-async-await.js
@@ -0,0 +1,10 @@
+const doWork = async () => {
+
+}
+
+
+doWork().then((result) => {
+    console.log('result', result)
+}).catch((e) => {
+    console.log('e', e)
+})
--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('65036eb37e2e4dab7a763adb').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('65036eb37e2e4dab7a763adb').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false})
+    return count
+}
+
+deleteTaskAndCount('650363134f0abe1f1cb1f1ef').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
