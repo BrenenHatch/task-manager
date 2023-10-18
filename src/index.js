b13993bc0d4645d3d73a4852eba855fd54752a98
Brenen Hatch <brenen.hatch21@gmail.com>
2023-10-18 13:38:13 -0400

    Added ability to add in profile picture

--- src/index.js
+++ src/index.js
@@ -6,36 +6,10 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-//     next()
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
-})  
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-    // const task = await Task.findById('651d854f96b1ee0c33c94478');
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-
-//     const user = await User.findById('651d815cc63827cdabe7cab1');
-
-//     if (user) {
-//         await user.populate( { path: 'tasks', model: Task } ) 
-//         console.log(user.tasks);
-//     } else {
-//         console.log('User not found');
-//     }
-// }
-
-// main()
+})
--- src/models/user.js
+++ src/models/user.js
@@ -47,7 +47,10 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true
     }
-  }]
+  }],
+  avatar: {
+    type: Buffer
+  }
 }, {
   timestamps: true,
 })
@@ -80,6 +83,7 @@ userSchema.methods.toJSON = function () {
 
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
 
   return userObject;
 };
@@ -112,6 +116,6 @@ userSchema.pre('deleteOne', { document: true }, async function(next) {
   next()
 })
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
